e8a06c6f79fd8b96de00615c5b91ed17da0b4a47
Teemu Kontio <teemu@t-palvelut.net>
2015-06-05 12:42:18 +0300

    added json script support

--- api/src/index.js
+++ api/src/index.js
@@ -4,6 +4,10 @@ var path            = require("path");
 var shell           = require('shelljs');
 var bodyParser      = require('body-parser');
 var cookieParser    = require('cookie-parser');
+var http            = require('http-request');
+
+// Timers
+var timeouts = [];
 
 // Constants
 var PORT = 8003;
@@ -45,45 +49,50 @@ app.locals.limit = {
     delayvariance: ''
 }
 
-app.put('/limit', function (req, res) {
+function setLimit(json){
+    console.log(json);
     var command = 'sudo tc qdisc change dev lo root netem ';
     /*When the user has NOT inputted any values to any of the fields in configure - index.jade
     let's assume that they want to clear their loss and delay, and reset the lingering variables*/
-    if (req.body.loss == "" && req.body.delay == "" && req.body.delayvariance == "") {
+    if (json.loss == "" && json.delay == "" && json.delayvariance == "") {
         app.locals.limit.loss = '';
         app.locals.limit.delay = '';
         app.locals.limit.delayvariance = '';
     }
     /*Do not allow loss-values over 100% or under 0%*/
-    if (req.body.loss != "" && req.body.loss >= 0 && req.body.loss <= 100) {
-        app.locals.limit.loss = req.body.loss;
-        command += 'loss ' + req.body.loss + '% ';
+    if (json.loss != "" && json.loss >= 0 && json.loss <= 100) {
+        app.locals.limit.loss = json.loss;
+        command += 'loss ' + json.loss + '% ';
     }
     else if (app.locals.limit.loss != '') {
         command += 'loss ' + app.locals.limit.loss + '% ';
     }
     /*Do not allow negative delay values*/
-    if (req.body.delay != "" && req.body.delay >= 0) {
-        app.locals.limit.delay = req.body.delay;
-        command += 'delay ' + req.body.delay + 'ms ';
+    if (json.delay != "" && json.delay >= 0) {
+        app.locals.limit.delay = json.delay;
+        command += 'delay ' + json.delay + 'ms ';
     }
     else if (app.locals.limit.delay != '') {
         command += 'delay ' + app.locals.limit.delay + 'ms ';
     }
     /*Don't execute unless the user has inputted a value for delay*/
-    if (req.body.delayvariance != "" && req.body.delay != "") {
-        app.locals.limit.delayvariance = req.body.delayvariance;
-        command += req.body.delayvariance + 'ms ';
+    if (json.delayvariance != "" && json.delay != "") {
+        app.locals.limit.delayvariance = json.delayvariance;
+        command += json.delayvariance + 'ms ';
     }
     else if (app.locals.limit.delayvariance != '') {
         command += app.locals.limit.delayvariance + 'ms ';
     }
-    if(parseInt(req.body.jitter) === 1){
-        command += 'distribution ' + req.body.distribution;
+    if(parseInt(json.jitter) === 1){
+        command += 'distribution ' + json.distribution;
     }
     if (command != 'sudo tc qdisc change dev lo root netem ') {
         shell.exec(command, {silent:true});
     }
+}
+
+app.put('/limit', function (req, res) {
+    setLimit(req.body);
     var status = shell.exec('sudo tc qdisc show', {silent:true}).output;
     res.json(status);
 });
@@ -105,7 +114,21 @@ app.put('/target', function (req, res) {
 });
 
 app.put('/script', function(req, res){
-    res.send(400);
+    for (var i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
+    }
+    timeouts = [];
+
+    var jsonscript = JSON.parse(req.body.jsonscript);
+    var seconds = Object.keys(jsonscript);
+    console.log('New script with timers at: ' +seconds.toString());
+    seconds.forEach(function(i){
+        timeouts.push(setTimeout(function(){
+            console.log('Delayed limit trigger ' + i + ' seconds');
+            setLimit(jsonscript[i]);
+        },i*1000));
+    });
+    res.json('New script with timers at: ' + seconds.toString());
 });
 
 app.listen(PORT);
