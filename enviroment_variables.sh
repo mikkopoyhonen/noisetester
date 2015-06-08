1c5152ab2f8ebb3a466d56dc9f9c013a3ef4bb56
Teemu Kontio <teemu@t-palvelut.net>
2015-06-08 14:54:31 +0300

    fixed enviroment variable generator

--- enviroment_variables.sh
+++ enviroment_variables.sh
@@ -2,7 +2,7 @@
 vagrantid=`vagrant global-status | grep running | cut -c-8`
 myip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 
-command="export API_URL=http://$myip:9002/api && export IO_URL=http://$myip:9001"
+command="export API_URL=http://$myip:8001/api && export IO_URL=http://$myip:8001"
 
 echo "**********************************************************************"
 echo "* GO TO YOUR CONTRIBOARD GULP TAB AND PASTE TO THE FOLLOWING COMMAND *"
