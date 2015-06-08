27da5ca8c3df9deca51657f7eda7627e2645707c
Teemu Kontio <teemu@t-palvelut.net>
2015-06-08 12:32:21 +0300

    added env variable to script

--- build_remove_run.sh
+++ build_remove_run.sh
@@ -2,6 +2,6 @@ sudo docker stop contriboard-noise
 sudo docker rm contriboard-noise
 sudo docker rmi temetz/contriboard-noise
 sudo docker build -t temetz/contriboard-noise .
-sudo docker run -i -t -d -p 8001:8001 -p 8002:8002 --privileged --name contriboard-noise temetz/contriboard-noise
+sudo docker run -i -t -d -e "NOISEPORT=8002" -p 8001:8001 -p 8002:8002 --privileged --name contriboard-noise temetz/contriboard-noise
 echo "Remember to set your API_URL and IO_URL enviroment variables"
 echo "Run: sh enviroment_variables.sh for easy copy-paste string of variables"
