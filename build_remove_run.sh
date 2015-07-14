717270407ad5ee32e2e3db060dd535b061481c42
Teemu Kontio <G2710@student.jamk.fi>
2015-07-14 11:54:06 +0300

    Changed container owner

--- build_remove_run.sh
+++ build_remove_run.sh
@@ -1,6 +1,6 @@
 sudo docker stop contriboard-noise
 sudo docker rm contriboard-noise
-sudo docker rmi temetz/contriboard-noise
-sudo docker build -t temetz/contriboard-noise .
-sudo docker run -i -t -d -e "NOISEPORT=8002" -p 80:8001 -p 8002:8002 --privileged --name contriboard-noise temetz/contriboard-noise
+sudo docker rmi N4SJAMK/contriboard-noise
+sudo docker build -t N4SJAMK/contriboard-noise .
+sudo docker run -i -t -d -e "NOISEPORT=8002" -p 80:8001 -p 8002:8002 --privileged --name contriboard-noise N4SJAMK/contriboard-noise
 echo "Next: run: sh noise_start_contriboard.sh"
